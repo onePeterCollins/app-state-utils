ea7fce61a095f592f3e406a07b26e41285caf414
onePeterCollins <onepetercollins@gmail.com>
2021-09-02 14:23:00 +0400

    Updated type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,1 @@
-export type MutationHelper = {
-    createEntry: object
-    updateValue: object
-    clearValue: object
-    deleteEntry: object
-    logState: object
-};
-
 export function MutationHelper (state: object): object;
